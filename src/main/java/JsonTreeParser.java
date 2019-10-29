feb3e0ae60800f56eed7740d0f735a3d5bba0396
farkaspeti <farkas.peti97@gmail.com>
2019-10-29 16:24:08 +0100

    treeParser method implemented

--- src/main/java/JsonTreeParser.java
+++ src/main/java/JsonTreeParser.java
@@ -1,2 +1,15 @@
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
 public class JsonTreeParser {
+    
+    public void treeParser(String json) throws JsonParseException, IOException {
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(json);
+        
+    }
 }
