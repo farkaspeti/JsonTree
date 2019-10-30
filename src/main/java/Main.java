0d0e10425df7f21d1673fc351c0a00f9da00bbce
farkaspeti <farkas.peti97@gmail.com>
2019-10-30 14:48:22 +0100

    deleted JsonTreeParser class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
 public class Main {
     
-    public static void main(String[] args) {
-        JsonTreeParser jsonTreeParser = new JsonTreeParser();
-        jsonTreeParser.treeParser("/home/farkas/Documents/Java+Web/JsonTree");
+    public static void main(String[] args) throws IOException, ParseException {
+        JsonFileReader jsonFileReader = new JsonFileReader();
+        jsonFileReader.fileReader("vehicles.json");
     }
 }
