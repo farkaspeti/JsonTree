a7eb95c5b30a4f153c1567dab2b14f521b46210d
farkaspeti <farkas.peti97@gmail.com>
2019-10-30 14:46:16 +0100

    fileReader implemented

--- src/main/java/JsonFileReader.java
+++ src/main/java/JsonFileReader.java
@@ -1,9 +1,19 @@
+import org.json.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class JsonFileReader {
     
     
-    public String fileReader(String path) {
-        String file = " ";
-        
-        return file;
+    public JSONArray fileReader(String fileName) throws IOException, ParseException {
+        File file = new File(fileName);
+        JSONParser parser = new JSONParser();
+        return (JSONArray) parser.parse(new FileReader(file));
     }
 }
